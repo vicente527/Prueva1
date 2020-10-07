23181cd4f687b31fd71418d72547a806e886d54b
Vicente Zuñiga <Vicente Zuiga@DESKTOP-M7S5V5K>
2020-10-07 15:58:28 -0500

    1.3

---
+++ src/VentanaPrueva.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Vicente Zuñiga
+ */
+import javax.swing.*;
+
+public class VentanaPrueva {
+    public static void main(String[]args){
+        JFrame f= new JFrame("Mi ventana");
+        f.setSize(400, 300);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+    
+}
